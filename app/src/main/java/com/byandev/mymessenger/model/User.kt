eef6df4ac6fc12ac9c4646b2c3d550b953554753
byansanur <ratbyansa25@gmail.com>
2020-02-01 04:22:17 +0700

    adsd

--- app/src/main/java/com/byandev/mymessenger/model/User.kt
+++ app/src/main/java/com/byandev/mymessenger/model/User.kt
@@ -1,9 +1,9 @@
-package com.byandev.mymessenger.Model
+package com.letsbuildthatapp.kotlinmessenger.models
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 class User(val uid: String, val username: String, val profileImageUrl: String): Parcelable {
-    constructor(): this("","","")
+  constructor() : this("", "", "")
 }
